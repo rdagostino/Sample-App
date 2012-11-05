f9217c0d30be378683a0eb71e1219802f23ff5b5
rdagostino <codedbyryan@gmail.com>
2012-11-05 15:50:48 -0500

    Added routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 SampleApp::Application.routes.draw do
-  match '/contact', :to => 'pages#contact'
-  match '/about', 	:to => 'pages#about'
-  match '/help', 	:to => 'pages#help'
-  root :to => 'welcome#index'
+  match '/contact', 		:to => 'pages#contact'
+  match '/about',   		:to => 'pages#about'
+  match '/help',    		:to => 'pages#help'
+  match '/pages/home',		:to => 'pages#home'
+  
+  root :to => "pages#home"
   
   # The priority is based upon order of creation:
   # first created -> highest priority.
